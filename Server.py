ba46d643b3d7042b46f8e7c27c95db3cd911117a
oribendror <oribendror@mail.tau.ac.il>
2025-04-06 21:04:39 +0300

    added features to connect to client

--- Server.py
+++ Server.py
@@ -1,8 +1,7 @@
 import socket
 import sys
 
-#Create socket
-
+# Create socket
 def socket_create():
     try:
         global host
@@ -14,6 +13,7 @@ def socket_create():
     except socket.error as msg:
         print("Socket creation error: " + str(msg))
 
+# Bind socket
 def socket_bind():
     try:
         global host
@@ -23,4 +23,33 @@ def socket_bind():
         s.bind((host, port))
         s.listen(5)
     except socket.error as msg:
-        print("Socket binding error: " + str(msg))
+        print("Socket binding error: " + str(msg) + "\n" + "Retrying...")
+        socket_bind()
+
+# Establish a connection with client
+def socket_accept():
+    conn, address = s.accept()
+    print("Connection has been established | " + "IP " + address[0] + " | Port " + str(address[1]))
+    send_commands(conn)
+    conn.close()
+
+# Send commands
+def send_commands(conn):
+    while true:
+        cmd = input()
+        if cmd == 'quit':
+            conn.close()
+            sys.exit()
+        if len(str.encode(cmd)) > 0:
+            conn.send(str.encode(cmd))
+            client_response = str(conn.recv(1024), "utf-8")
+            print(client_response, end = "")
+
+
+def main():
+    socket_create()
+    socket_bind()
+    socket_accept()
+
+main()
+

